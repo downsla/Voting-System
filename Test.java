bcf746ef50fb507fadda6599d8f659cb88383827
SirMacKnight <74620708+SirMacKnight@users.noreply.github.com>
2021-06-07 03:18:42 -0500

    Update Test.java
    added a few features, depreciated hashFunction, cleaned variable names, still just a test file

--- Test.java
+++ Test.java
@@ -9,6 +9,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.RandomAccessFile;
 import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -19,23 +20,23 @@ import java.util.Set;
 
 public class Test {
 	
-	public static String getVUID(Random r, Set<String> keys) {
-		BigInteger id;
-		int[] bits = new int[] {32, 29, 27, 25, 18, 16, 15, 14, 12, 9, 2, 1};
+	public static String genVUID(Random r, Set<String> k) {
+		BigInteger bi;
+		int[] b = new int[] {32, 29, 27, 25, 18, 16, 15, 14, 12, 9, 2, 1};
 		while(true) {
-			id = new BigInteger("0");
-			for(int i = 0; i < bits.length; i++) {
-				BigInteger random = new BigInteger(bits[i], r);
-				id = id.add(random);
+			bi = new BigInteger("0");
+			for(int i = 0; i < b.length; i++) {
+				BigInteger random = new BigInteger(b[i], r);
+				bi = bi.add(random);
 			}
-			if(!keys.contains(hashFunction(id.toString()))) {
+			if(!k.contains(bi.toString())) {
 				break;
 			}
 	    }
-	    return String.format("%010d", id);
+	    return String.format("%010d", bi);
 	}
 	
-	public static String hashFunction(String data) {
+	public static String hashFunction(String data) { //unused
 		String hash = new String();
 		try {
 			MessageDigest md = MessageDigest.getInstance("SHA-256");
@@ -49,27 +50,27 @@ public class Test {
 		return hash;
 	}
 	
-	public static String[] lookUp(String h, HashMap<String, Long> m, File f) {
-		String data = new String();
+	public static String[] getLookup(String s, HashMap<String, Long> hm, File f) {
+		String a = new String();
 		try {
-			BufferedReader reader = new BufferedReader(new FileReader(f));
-			reader.skip(m.get(h));
-			data = reader.readLine();
-			reader.close();
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			br.skip(hm.get(s));
+			a = br.readLine();
+			br.close();
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
 			e.printStackTrace();
 		}
-		return data.split(",");
+		return a.split(",");
 	}
 	
-	public static long register(StringBuilder d, File f) {
+	public static long register(String s, File f) {
 		long l = f.length();
 		try {
-			BufferedWriter writer = new BufferedWriter(new FileWriter(f, true));
-			writer.write(d.toString());
-			writer.close();
+			BufferedWriter bw = new BufferedWriter(new FileWriter(f, true));
+			bw.write(s);
+			bw.close();
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
@@ -79,11 +80,11 @@ public class Test {
 	}
 	
 	public static HashMap<String, Long> loadHash(File f) {
-		HashMap<String, Long> m = new HashMap<String, Long>();
+		HashMap<String, Long> hm = new HashMap<String, Long>();
 		if(f.length() != 0) {
 			try {
 				ObjectInputStream inStream = new ObjectInputStream(new FileInputStream(f));
-				m = (HashMap<String, Long>) inStream.readObject();
+				hm = (HashMap<String, Long>) inStream.readObject();
 				inStream.close();
 			} catch(FileNotFoundException e) {
 				e.printStackTrace();
@@ -93,14 +94,57 @@ public class Test {
 				e.printStackTrace();
 			}
 		}
-		return m;
+		return hm;
 	}
 	
-	public static void saveHash(HashMap<String, Long> m, File f) {
+	public static void saveHash(HashMap<String, Long> hm, File f) {
 		try {
 			ObjectOutputStream outStream = new ObjectOutputStream(new FileOutputStream(f));
-			outStream.writeObject(m);
+			outStream.writeObject(hm);
 			outStream.close();
+		} catch(FileNotFoundException e) {
+			e.printStackTrace();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String formatRow(String[] sa) {
+		StringBuilder sb = new StringBuilder(sa[0]).append(',');
+		int[] p = new int[] {20, 20, 20, 20};
+		for(int i = 1; i < p.length; i++) {
+			char[] ch = new char[p[i] - sa[i].length()];
+			Arrays.fill(ch, ' ');
+			sb.append(ch).append(sa[i]).append(',');
+		}
+		for(int i = (p.length + 1); i < sa.length; i++) {
+			sb.append(sa[i]).append(',');
+		}
+		sb.append('\n');
+		return sb.toString();
+	}
+	
+	public static String formatCell(String s) {
+		char[] ch = s.toCharArray();
+		StringBuilder sb = new StringBuilder();
+		int i;
+		for(i = 0; i < ch.length; i++) {
+			if(ch[i] != ' ') {
+				break;
+			}
+		}
+		for(; i < ch.length; i++) {
+			sb.append(ch[i]);
+		}
+		return sb.toString();
+	}
+	
+	public static void editRow(String[] sa, String s, HashMap<String, Long> hm, File f) { //not tested
+		try {
+			RandomAccessFile raf = new RandomAccessFile(f, "rw");
+			raf.seek(hm.get(s));
+			raf.write(Arrays.toString(sa).getBytes());
+			raf.close();
 		} catch(FileNotFoundException e) {
 			e.printStackTrace();
 		} catch(IOException e) {
@@ -130,19 +174,34 @@ public class Test {
 		
 		HashMap<String, Long> map = loadHash(index);
 		
-		String s = new String(getVUID(new Random(), map.keySet()));
-		StringBuilder string = new StringBuilder(s);
-		string.append(",\"hello\",");
-		string.append("\"world\",");
-		string.append('\n');
+		String id = new String(genVUID(new Random(), map.keySet()));
+		String firstName = new String("JOHN");
+		String lastName = new String("DOE");
+		String street = new String("1234 ROAD ST");
+		String city = new String("CITY");
+		String state = new String("TX");
+		String zip = new String("123456");
+		String dob = new String("01022003");
+		String sex = new String("M");
+		String race = new String("W");
+		String valid = new String("0101202012312021");
 		
-		long len = register(string, file);	
+		String[] sArr = new String[] {id, lastName, firstName, street, city, state, zip, dob, sex, race, valid};
+		String s = formatRow(sArr);
+		long l = register(s, file);
 		
-		map.put(hashFunction(s), len);
+		StringBuilder name = new StringBuilder(lastName).append(firstName);
+		StringBuilder address = new StringBuilder(street).append(city).append(state).append(zip);
+		StringBuilder sb = new StringBuilder(name).append(address);
 		
-		saveHash(map, index);
+		if(!map.containsKey(sb.toString())) {
+			map.put(sb.toString(), l);
+			saveHash(map, index);
+		}
 		
-		System.out.println(Arrays.toString(lookUp(hashFunction(s), map, file)));
+		for(int i = 0; i < 10; i++) {
+			System.out.println(formatCell(getLookup(sb.toString(), map, file)[i]));
+		}
 		
 	}
 
