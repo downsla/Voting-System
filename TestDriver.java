3d2450cd68e9e41172f42ea0544405bec44a1844
terminatorlake <63393760+terminatorlake@users.noreply.github.com>
2021-06-12 18:13:33 -0500

    Added new methods for registering a new voter and generating test data

--- TestDriver.java
+++ TestDriver.java
@@ -7,6 +7,117 @@ public class TestDriver {
 		Ballot.loadData(state);
 		Candidate.loadData(state);
 	}
+
+	/*
+	This used for testing bulk registration data. Used the full names of everyone currently registered in CPSC 4360.
+	Full names are split, the address is Lamar University and DOB is set to 01012000. Sex and Race are set to O.
+ 	*/
+	public static void registerNewTestVoterData() {
+		String[] fullNames = new String[]{"KIRK SARRINE", "STEFAN ANDREI", "ERIC BARROW", "KALAN BONNETTE",
+				"COURTNEY BOWE", "HENRY DAO", "LIAM DOWNS", "JANUARIO FUENTES", "REAGAN GRIFFIS", "VIET-TUAN LUONG",
+				"MUHAMMAD MUBASHIR", "NICHOLAS MUGLESTON", "ADETOLA OLUKOYA", "CHRISLYN PETTUS"};
+		for(String name: fullNames){
+			String[] voterInfo = new String[11];
+			String[] splitName = name.split(" ");
+			String[] input = new String[]{splitName[0], splitName[1], "4400 ML KING JR PKWY", "BEAUMONT", "TX", "77705",
+			"01012000","O","O"};
+			//voterInfo lastName is at 1
+			System.arraycopy(input, 0, voterInfo, 1, input.length);
+
+			String searchK = Voter.getSearchKeyNAD(voterInfo); //gets string to search with
+			long locInFile; //saves location in voterFile of voterInfo
+
+			System.out.println("voterFullName: " + name);
+			System.out.print("voterInfo: " + Arrays.toString(voterInfo));
+
+			if(Voter.checkKeyNAD(searchK)) {
+				System.out.println("\nVoter already database, registration not required\n");
+			}
+			else {
+				System.out.println("\nVoter not in database, registering voter");
+				Voter.register(voterInfo); //registers voter, returns complete info
+				locInFile = Voter.getKeyValNAD(searchK);
+				if(Voter.checkKeyNAD(searchK))
+					voterInfo = Voter.lookup(locInFile);
+					System.out.println("Voter now in database, registration successful\n");
+					System.out.println("voterInfo: " + Arrays.toString(voterInfo) + "\n"); //test print
+				}
+		}
+	}
+
+	/* Get user input from the terminal to register a new voter. If the voter is already in the database,
+	the voter is not added. Otherwise, new voter info is added to database and written to voters.csv
+	 */
+	public static void registerNewVoter()
+	{
+		Scanner scan = new Scanner(System.in);
+		// Used for terminal output
+		String[] fields = new String[]{"First name (20 char max)", "Last name (20 char max)", "Street (20 char max)",
+				"City (20 char max)", "State (2 char max)", "Zip code (5 char max)", "DOB (mmddyyyy)",
+				"Sex (1 char max)", "Race (1 char max)"};
+		String[] voterInfo = new String[11]; // Used to build string to add to database and voter.csv
+		System.out.println("Enter voter info");
+
+		// Loops through the 9 fields required to enter a new voter in the database
+		for(int i = 0; i < 9; i++) {
+			System.out.print(fields[i] + ": ");
+
+			// If statements are used if the input is greater than the allowed max
+			// While loop continues until user enters in correct info
+			// trim() is for leading whitespaces and toUppperCase() for capitalizing all characters
+			if (i <= 3) {
+				voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				while (voterInfo[i + 1].length() > 20) {
+					System.out.print("Max chars reached, please re-enter: ");
+					voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				}
+			} else if (i == 4) {
+				voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				while (voterInfo[i + 1].length() > 2) {
+					System.out.print("Max chars reached, please re-enter: ");
+					voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				}
+			} else if (i == 5) {
+				voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				while (voterInfo[i + 1].length() > 5) {
+					System.out.println("Max chars reached, please re-enter: ");
+					voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				}
+			} else if (i == 6) {
+				voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				while (voterInfo[i + 1].length() > 8) {
+					System.out.println("Max chars reached, please re-enter: ");
+					voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				}
+			}
+			else if (i == 7 || i == 8) {
+				voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				while (voterInfo[i + 1].length() > 1) {
+					System.out.println("Max chars reached, please re-enter: ");
+					voterInfo[i + 1] = scan.nextLine().trim().toUpperCase();
+				}
+			}
+		}
+		String searchKey = Voter.getSearchKeyNAD(voterInfo); //gets string to search with
+		long locInFile; //saves location in voterFile of voterInfo
+
+		// Check for if the voter is already in the database
+		if(Voter.checkKeyNAD(searchKey))
+			System.out.println("\nVoter already database, registration not required");
+		else {
+			System.out.println("Voter not in database, registering voter");
+			Voter.register(voterInfo); //registers voter, returns complete info
+			locInFile = Voter.getKeyValNAD(searchKey); // Used for output at the end of else
+
+			// Check to ensure the new voter info was added
+			if(Voter.checkKeyNAD(searchKey)) {
+				voterInfo = Voter.lookup(locInFile); // Pulls the location from the voters.csv file
+				System.out.println("Voter now in database, registration successful\n");
+				System.out.println("voterInfo: " + Arrays.toString(voterInfo) + "\n"); //test print
+			}
+		}
+
+	}
 	
 	public static void main(String[] args) {
 		
